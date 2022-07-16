7ed28759f86b69a091c11b0cadb0a525fa4e3526
Horácio Barreto <horaciovlogs@gmail.com>
2022-07-16 13:07:10 -0300

    site (versão1)

---
+++ js/js/imc/js.js
@@ -0,0 +1,24 @@
+function calcular(){
+    var peso = document.querySelector("#number").value;
+    var altura = document.querySelector("#number1").value / 100;
+    var nome = document.getElementById("nome").value;
+    alert("Olá, " + nome);
+    alert("Seu IMC é de " + peso/(altura*altura))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
